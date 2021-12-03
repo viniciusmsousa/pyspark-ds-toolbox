fa2f266bc32a9762ea8538ff6f106a1960954a5b
viniciusmsousa <vinisousa04@gmail.com>
2021-12-03 14:06:08 -0300

    refact: add causal_inference.ps_matching import

--- pyspark_ds_toolbox/causal_inference/__init__.py
+++ pyspark_ds_toolbox/causal_inference/__init__.py
@@ -3,3 +3,5 @@
 Sub-package dedicated to functionalities related to the field of causal inference. 
 For a introduction on the subject see: https://mixtape.scunning.com/introduction.html#what-is-causal-inference
 """
+
+from pyspark_ds_toolbox.causal_inference import ps_matching
